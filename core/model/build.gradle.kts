f3db29d30f40e2f8e724497b767886f8edfe084f
Heonbyeong <abh0920one@gmail.com>
2024-05-17 14:34:10 +0900

    moneymong-372 feat: agency 관련 model dependency 추가

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+dependencies {
+    implementation(libs.retrofit.gson.converter)
 }
