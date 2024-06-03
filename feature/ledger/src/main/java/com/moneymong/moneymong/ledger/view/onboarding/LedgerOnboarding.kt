0f7197aaa9d3bc2c435a0a2dd10326ef094dc60f
jhg3410 <jhg34100@naver.com>
2024-06-03 22:06:24 +0900

    MONEYMONG-374 feat: 장부 온보딩 열람 기간 tooltip 구현

--- feature/ledger/src/main/java/com/moneymong/moneymong/ledger/view/onboarding/LedgerOnboarding.kt
+++ feature/ledger/src/main/java/com/moneymong/moneymong/ledger/view/onboarding/LedgerOnboarding.kt
@@ -1,12 +1,18 @@
 package com.moneymong.moneymong.ledger.view.onboarding
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import com.moneymong.moneymong.ledger.view.LedgerDefaultDateRow
 import java.time.LocalDate
@@ -29,16 +35,28 @@ internal fun LedgerOnboarding(
 
     Popup {
         Box(modifier = modifier) {
-            LedgerDefaultDateRow(
-                modifier = Modifier.offset {
-                    IntOffset(
-                        x = dateComponent.offset.x.toInt(),
-                        y = dateComponent.offset.y.toInt()
-                    )
-                },
-                currentDate = currentDate,
-                onAddMonthFromCurrentDate = {}
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset {
+                        IntOffset(
+                            x = dateComponent.offset.x.toInt(),
+                            y = dateComponent.offset.y.toInt()
+                        )
+                    },
+            ) {
+                LedgerDefaultDateRow(
+                    currentDate = currentDate,
+                    onAddMonthFromCurrentDate = {}
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                LedgerOnboardingToolTip(
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    text = "장부 열람 기간을 설정할 수 있어요!",
+                    verticalArrowPosition = VerticalArrowPosition.TOP,
+                    horizontalArrowPosition = HorizontalArrowPosition.CENTER
+                )
+            }
         }
     }
 }
