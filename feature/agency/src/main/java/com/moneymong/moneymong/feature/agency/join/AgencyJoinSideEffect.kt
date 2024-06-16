605d8b11f54a58fb9b131df651c77144acee3e01
jhg3410 <jhg34100@naver.com>
2024-06-16 17:06:43 +0900

    MONEYMONG-471 refactor: Reformat code

--- feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/join/AgencyJoinSideEffect.kt
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/join/AgencyJoinSideEffect.kt
@@ -2,5 +2,5 @@ package com.moneymong.moneymong.feature.agency.join
 
 import com.moneymong.moneymong.common.base.SideEffect
 
-sealed class AgencyJoinSideEffect : SideEffect{
+sealed class AgencyJoinSideEffect : SideEffect {
 }
