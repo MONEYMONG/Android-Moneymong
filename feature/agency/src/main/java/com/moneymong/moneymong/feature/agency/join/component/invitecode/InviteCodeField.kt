2efcaa1999f482bc87f6f5c9d24f8c533cd0d74d
jhg3410 <jhg34100@naver.com>
2024-06-16 17:09:24 +0900

    MONEYMONG-471 feat: 초대 코드 UI 구현

---
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/join/component/invitecode/InviteCodeField.kt
@@ -0,0 +1,147 @@
+package com.moneymong.moneymong.feature.agency.join.component.invitecode
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.moneymong.moneymong.design_system.R
+import com.moneymong.moneymong.design_system.theme.Black
+import com.moneymong.moneymong.design_system.theme.Heading1
+import com.moneymong.moneymong.feature.agency.join.component.CODE_MAX_SIZE
+
+@Composable
+internal fun InviteCodeField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    isError: Boolean = false
+) {
+    BasicTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        decorationBox = {
+            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                val containerModifier = Modifier
+                    .aspectRatio(45 / 72f)
+                    .weight(1f)
+
+                if (isError) {
+                    value.forEach { char ->
+                        InviteNumberFieldContainer(
+                            modifier = containerModifier,
+                            text = char,
+                            state = InviteCodeFieldState.ERROR
+                        )
+                    }
+                } else {
+                    value.forEach { char ->
+                        InviteNumberFieldContainer(
+                            modifier = containerModifier,
+                            text = char,
+                            state = InviteCodeFieldState.FILLED
+                        )
+                    }
+                    if (value.length < CODE_MAX_SIZE) {
+                        InviteNumberFieldContainer(
+                            modifier = containerModifier,
+                            text = ' ',
+                            state = InviteCodeFieldState.FOCUSED
+                        )
+                    }
+                    repeat(CODE_MAX_SIZE - value.length - 1) {
+                        InviteNumberFieldContainer(
+                            modifier = containerModifier,
+                            text = ' ',
+                            state = InviteCodeFieldState.EMPTY
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+
+@Composable
+private fun InviteNumberFieldContainer(
+    modifier: Modifier = Modifier,
+    text: Char,
+    state: InviteCodeFieldState
+) {
+
+    val baseContainer: @Composable (Modifier, Color, Color) -> Unit =
+        { containerModifier, backgroundColor, borderColor ->
+            Box(
+                modifier = containerModifier
+                    .clip(RoundedCornerShape(size = 8.dp))
+                    .background(color = backgroundColor)
+                    .run {
+                        border(
+                            width = 1.dp,
+                            color = borderColor,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = text.toString(),
+                    color = Black,
+                    style = Heading1,
+                )
+            }
+        }
+
+
+    when (state) {
+        is InviteCodeFieldState.FILLED -> {
+            Image(
+                modifier = modifier,
+                painter = painterResource(id = R.drawable.img_code),
+                contentDescription = null
+            )
+        }
+
+        is InviteCodeFieldState.EMPTY -> {
+            baseContainer(
+                modifier,
+                state.backgroundColor,
+                state.borderColor
+            )
+        }
+
+        is InviteCodeFieldState.FOCUSED -> {
+            baseContainer(
+                modifier,
+                state.backgroundColor,
+                state.borderColor
+            )
+        }
+
+        is InviteCodeFieldState.ERROR -> {
+            baseContainer(
+                modifier,
+                state.backgroundColor,
+                state.borderColor
+            )
+        }
+    }
+}
