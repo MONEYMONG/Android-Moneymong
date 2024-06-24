bf35219fabc3a268926fd0975cb10710a3a13c0f
Heon <abh0920one@gmail.com>
2024-06-24 22:10:55 +0900

    chore: version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.moneymong.moneymong"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.1"
+        versionCode = 11
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
