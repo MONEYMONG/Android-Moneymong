06c7c453ef92e3d42665f7e084be93f7fea63db1
jhg3410 <jhg34100@naver.com>
2024-07-12 00:21:57 +0900

    moneymong-323 refactor: LoginRemoteDataSource 내부 로직 분리
    datasource 는 데이터 원천
    repository 는 datasource 에서 가져온 데이터 활용
    usecase 에서 repository 활용

--- data/src/main/java/com/moneymong/moneymong/data/datasource/login/LoginRemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.moneymong.moneymong.data.datasource.login
-
-import com.moneymong.moneymong.model.sign.LoginType
-
-
-interface LoginRemoteDataSource {
-    suspend fun postAccessToken(type: LoginType, accessToken: String): Result<Unit>
-}
