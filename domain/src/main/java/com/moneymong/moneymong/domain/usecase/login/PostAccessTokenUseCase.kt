9f37a712b8ebfb463205feae3bdfd0494281c2a7
jhg3410 <jhg34100@naver.com>
2024-07-12 00:21:57 +0900

    moneymong-323 refactor: LoginRemoteDataSource 내부 로직 분리
    datasource 는 데이터 원천
    repository 는 datasource 에서 가져온 데이터 활용
    usecase 에서 repository 활용

--- domain/src/main/java/com/moneymong/moneymong/domain/usecase/login/PostAccessTokenUseCase.kt
+++ domain/src/main/java/com/moneymong/moneymong/domain/usecase/login/PostAccessTokenUseCase.kt
@@ -1,13 +1,18 @@
 package com.moneymong.moneymong.domain.usecase.login
 
-import com.moneymong.moneymong.domain.repository.LoginRepository
+import com.moneymong.moneymong.domain.repository.TokenRepository
+import com.moneymong.moneymong.domain.repository.user.UserRepository
 import com.moneymong.moneymong.model.sign.LoginType
+import com.moneymong.moneymong.model.sign.TokenResponse
 import javax.inject.Inject
 
 class PostAccessTokenUseCase @Inject constructor(
-    private val loginRepository: LoginRepository,
+    private val tokenRepository: TokenRepository,
+    private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(type: LoginType, accessToken: String): Result<Unit> {
-        return loginRepository.postAccessToken(type = type, accessToken = accessToken)
+    suspend operator fun invoke(type: LoginType, accessToken: String): Result<TokenResponse> {
+        return tokenRepository.postAccessToken(type = type, accessToken = accessToken).onSuccess {
+            userRepository.saveUserInfo()
+        }
     }
 }
