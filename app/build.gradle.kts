64b3ba36f1a629774ec5d97442dacfe546c19578
Heon <abh0920one@gmail.com>
2024-08-13 01:23:23 +0900

    INCREMENT VERSION CODE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.moneymong.moneymong"
         minSdk = 24
         targetSdk = 34
-        versionCode = 14
+        versionCode = 15
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
