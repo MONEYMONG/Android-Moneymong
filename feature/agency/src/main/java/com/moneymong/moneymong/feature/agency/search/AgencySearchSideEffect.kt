5d54fd46cfa6cdd7c0a49ddca193cf7f963d0b17
Heon <abh0920one@gmail.com>
2024-08-16 12:07:10 +0900

    feat: 의견 제안 클릭 이벤트 적용

--- feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/search/AgencySearchSideEffect.kt
+++ feature/agency/src/main/java/com/moneymong/moneymong/feature/agency/search/AgencySearchSideEffect.kt
@@ -5,4 +5,5 @@ import com.moneymong.moneymong.common.base.SideEffect
 sealed interface AgencySearchSideEffect : SideEffect {
     data class NavigateToRegister(val isUniversityStudent: Boolean) : AgencySearchSideEffect
     data class NavigateToAgencyJoin(val agencyId: Long) : AgencySearchSideEffect
+    data object FollowAsk : AgencySearchSideEffect
 }
