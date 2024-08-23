e7e4a733979fcfde4f2ca2ceb39085b10eda7fd4
Heon <abh0920one@gmail.com>
2024-08-23 18:44:25 +0900

    chore: version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 15
-        versionName = "1.2.0"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
