731ee55e6c39f4d7dedd08332f1a494f324339ce
Heon <abh0920one@gmail.com>
2024-10-01 17:48:20 +0900

    chore: version code up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.moneymong.moneymong"
         minSdk = 24
         targetSdk = 34
-        versionCode = 24
+        versionCode = 25
         versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
