ae0ce1a00c6b580f3c4325dfedddca2c74df9beb
Anka <runner@Mac-1727770251630.local>
2024-10-01 08:19:58 +0000

    INCREMENT VERSION CODE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.moneymong.moneymong"
         minSdk = 24
         targetSdk = 34
-        versionCode = 20
+        versionCode = 24
         versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
