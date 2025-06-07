2c518ba773df2de1a5e2dff2190171d22b5f5e24
Heon <abh0920one@gmail.com>
2025-06-07 22:44:12 +0900

    chore: distribute fastfile 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.moneymong.moneymong"
         minSdk = 24
         targetSdk = 34
-        versionCode = 29
+        versionCode = 30
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
