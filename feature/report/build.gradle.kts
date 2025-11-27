bdfedbb95151faafe7d16dbee731f049d3a99c79
heejik <jhg34100@naver.com>
2025-11-27 21:56:17 +0900

    report 모듈 build.gradle 수정(dependencies and plugins)

--- feature/report/build.gradle.kts
+++ feature/report/build.gradle.kts
@@ -1,38 +1,18 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.moneymong.android.library.compose)
+    alias(libs.plugins.moneymong.android.feature)
+    alias(libs.plugins.moneymong.android.hilt)
 }
 
 android {
     namespace = "com.moneymong.moneymong.report"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
+    implementation(projects.core.model)
+    implementation(projects.core.designSystem)
+    implementation(projects.core.ui)
+    implementation(projects.domain)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.appcompat)
@@ -40,4 +20,8 @@ dependencies {
     testImplementation(libs.junit4)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
+
+    implementation(libs.orbit.core)
+    implementation(libs.orbit.compose)
+    implementation(libs.orbit.viewModel)
 }
