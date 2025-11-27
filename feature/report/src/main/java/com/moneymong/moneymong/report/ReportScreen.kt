da3f61cb577a57adebeb851983a00ee152c00072
heejik <jhg34100@naver.com>
2025-11-27 22:39:14 +0900

    reportScreen 상단 요약 View 구현

--- feature/report/src/main/java/com/moneymong/moneymong/report/ReportScreen.kt
+++ feature/report/src/main/java/com/moneymong/moneymong/report/ReportScreen.kt
@@ -1,13 +1,40 @@
 package com.moneymong.moneymong.report
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.moneymong.moneymong.design_system.theme.Blue04
+import com.moneymong.moneymong.design_system.theme.Body2
+import com.moneymong.moneymong.design_system.theme.Gray01
+import com.moneymong.moneymong.design_system.theme.Gray06
+import com.moneymong.moneymong.design_system.theme.Gray10
+import com.moneymong.moneymong.design_system.theme.Heading1
+import com.moneymong.moneymong.design_system.theme.Heading5
 import com.moneymong.moneymong.design_system.theme.MMHorizontalSpacing
+import com.moneymong.moneymong.design_system.theme.White
 import com.moneymong.moneymong.report.component.ReportTopBar
+import com.moneymong.moneymong.ui.toWonFormat
+import com.moneymong.moneymong.design_system.R as MDSR
 
 
 @Composable
@@ -24,8 +51,8 @@ private fun ReportScreen(
     modifier: Modifier = Modifier
 ) {
 
-    Column(modifier = modifier) {
-        ReportTopBar { /* todo */ }
+    Column(modifier = modifier.background(color = Gray01)) {
+        ReportTopBar(modifier = Modifier.fillMaxWidth()) { /* todo */ }
         ReportSummary(modifier = Modifier.padding(horizontal = MMHorizontalSpacing))
         ReportContent()
     }
@@ -38,7 +65,66 @@ private fun ReportSummary(
     income: Int = 100000, // todo
     expense: Int = 100000 // todo
 ) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(20.dp))
+            .background(color = White)
+            .padding(vertical = 20.dp, horizontal = 24.dp),
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Blue04)) {
+                        append("$balance")
+                    }
+                    append("원\n남아 있어요!")
+                },
+                color = Gray10,
+                style = Heading5
+            )
+            Image(
+                modifier = Modifier.size(80.dp),
+                painter = painterResource(MDSR.drawable.ic_record),
+                contentDescription = "레포트 이미지"
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            SummaryItem(amount = income)
+            SummaryItem(amount = expense)
+        }
+    }
+}
 
+@Composable
+private fun SummaryItem(
+    modifier: Modifier = Modifier,
+    amount: Int
+) {
+    Column(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(size = 8.dp))
+            .background(color = Gray01)
+            .padding(vertical = 12.dp, horizontal = 28.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "총 수입",
+            color = Gray06,
+            style = Body2
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            text = "+${amount.toString().toWonFormat()}원",
+            color = Gray10,
+            style = Heading1
+        )
+    }
 }
 
 @Composable
